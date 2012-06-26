fd93c4dba6a6a93321ac1c8934980f8c8708495b
Andreas kaiser <disko@binary-punks.com>
2012-06-26 14:20:54 +0200

    initial version

---
+++ kotti_website/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from kotti.static import edit_needed
+from kotti.static import view_needed
+from static import css
+
+
+def includeme(config):
+    config.add_static_view('static-kotti_website', 'kotti_website:static')
+    edit_needed.add(css)
+    view_needed.add(css)
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+version = '0.1'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = ['kotti>=0.7dev',
+            'kotti_calendar',
+            'kotti_gallery',
+            'kotti_video',
+            'pyramid_debugtoolbar',
+            'waitress', ]
+
+setup(name='kotti_website',
+      version=version,
+      description="Kotti CMS Website",
+      long_description="""Website of the Kotti Content Management System""",
+      classifiers=["Programming Language :: Python",
+                   "Framework :: Pylons",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Topic :: Internet :: WWW/HTTP :: WSGI :: Application", ],
+      keywords='web pyramid pylons',
+      author='Andreas Kaiser',
+      author_email='disko@binary-punks.com',
+      url='http://www.kotti-cms.org/',
+      license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""
+        [paste.app_factory]
+        main = kotti_website:main
+        [fanstatic.libraries]
+        ffl = kotti_website.static:lib
+      """,
+      )
